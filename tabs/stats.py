7a6c103541369a0ee57e1c18e29966377ccf1e9c
Jonathan Kim <jonathankim1626@gmail.com>
2025-02-27 22:53:58 -0800

    Update on widgets and stats

--- tabs/stats.py
+++ tabs/stats.py
@@ -1,15 +1,85 @@
 from dash import dcc, html, Input, Output, callback
-from widgets import cont_population, year_population
-from charts import create_stats_chart
+from widgets import dropdown_analysis_type, linreg_dropdown_x, linreg_dropdown_y, summarystat_dropdown, pca_dropdown
+from charts import create_stats_chart, create_summary_statistics, create_summary_table, create_lr_chart
+import plotly.express as px
 
-stats_layout = dcc.Tab([
-    html.Br(), "Continent", cont_population, "Year", year_population, html.Br(),
-    dcc.Graph(id="Stats and Plots")
-], label="Stats and Plots")
+stats_layout = dcc.Tab(
+    label="Stats and Plots",
+    children = [
+        html.Br(),
+        html.Label("Analysis Type"), 
+        dropdown_analysis_type,
+        html.Br(),
+        html.Div(id="dropdown-container", children=[summarystat_dropdown, linreg_dropdown_x, linreg_dropdown_y, pca_dropdown]),
+        html.Br(),
+        html.Div(id="summarystats-container"),
+        dcc.Graph(id="Stats and Plots"),
+
+        # Store components (hold selected values)
+        dcc.Store(id="selected-x"),
+        dcc.Store(id="selected-y"),
+        dcc.Store(id="selected-summary"), 
+        dcc.Store(id="selected-pca")
+    ]
+)
+
+# Handle dropdown display
+@callback(
+    Output("dropdown-container", "children"),
+    Input("analysis_type", "value")
+)
+def update_dropdowns(analysis_type):
+    if analysis_type == "summarystat":
+        return [
+            html.Label("Select Column for Summary Statistics"),
+            summarystat_dropdown
+        ]
+    elif analysis_type == "linreg":
+        return html.Div([
+            html.Label("Select X Variable"),
+            linreg_dropdown_x,
+            html.Br(),
+            html.Label("Select Y Variable"),
+            linreg_dropdown_y
+        ])
+    elif analysis_type == "pca":
+        return html.Div([
+            html.Label("Select PCA variables"),
+            pca_dropdown, 
+            html.Br()
+        ])
+    else:
+        return html.Div()
+    
+# Stores (fix dynamic dropdown issue)
+@callback(
+    Output("selected-summary", "data"),
+    Input("summarystat_dropdown", "value"),
+    prevent_initial_call=True
+)
+def update_summary_store(value):
+    return value
 
 @callback(
-    Output("Stats and Plots", "figure"),
-    [Input("cont_pop", "value"), Input("year_pop", "value")]
+    Output("selected-x", "data"),
+    Input("linreg_x", "value"),
+    prevent_initial_call=True
 )
-def update_stats_chart(continent, year):
-    return create_stats_chart(continent, year)
+def update_x_store(value):
+    return value
+
+@callback(
+    Output("selected-y", "data"),
+    Input("linreg_y", "value"),
+    prevent_initial_call=True
+)
+def update_y_store(value):
+    return value
+
+@callback(
+    Output("selected-pca", "data"),
+    Input("pca_dropdown", "value"),
+    prevent_initial_call=True
+)
+def update_pca_store(value):
+    return value
--- widgets.py
+++ widgets.py
@@ -1,5 +1,6 @@
 from dash import dcc
-from charts import gapminder_df
+from charts import gapminder_df, db_df
+import numpy as np
 
 continents = gapminder_df.Continent.unique()
 years = gapminder_df.Year.unique()
@@ -18,4 +19,43 @@ var_map = dcc.Dropdown(
     id="var_map",
     options=["Population", "GDP per Capita", "Life Expectancy"],
     value="Life Expectancy", clearable=False
+)
+
+# Stats page widgets
+dropdown_analysis_type = dcc.Dropdown(
+    id="analysis_type",
+    options=[
+        {'label': 'Linear Regression', 'value': 'linreg'}, 
+        {'label': 'PCA (Principal Component Analysis)', 'value': 'pca'},
+        {'label': 'Summary Statistics', 'value': 'summarystat'}
+    ],
+    value=None, 
+    placeholder='Select Analysis Type'
+)
+
+linreg_dropdown_x = dcc.Dropdown(
+    id="linreg_x", 
+    options=[{'label':col, 'value':col} for col in db_df.columns],
+    # value="Height_cm",
+    placeholder='Select x-axis variable'
+)
+
+linreg_dropdown_y = dcc.Dropdown(
+    id="linreg_y", 
+    options=[{'label':col, 'value':col} for col in db_df.columns],
+    # value="Height_cm",
+    placeholder='Select y-axis variable'
+)
+
+summarystat_dropdown = dcc.Dropdown(
+    id="summarystat_dropdown",
+    options=[{'label':col, 'value':col} for col in db_df[['Height_cm', 'BDiam_mm', 'DBH_1', 'DBH_2', 'DBH_3', 'DBH_4', 'DBH_5', 'DBH_6', 'DBH_7', 'DBH_8', 'Alive_new', 'Multistem_yn', 'Gopher_damage', 'Powderymildew_yn', 'Brownspots', 'Yellowspots', 'Observation_date', 'Site']]], # columns we can perform meaningful summary stats on
+    placeholder="Select column"
+)
+
+pca_dropdown = dcc.Dropdown(
+    id="pca_dropdown", 
+    options= [{'label':col, 'value':col} for col in db_df.columns],
+    multi=True,
+    placeholder="Select variables for CA analysis"
 )
