3ce751688f57123ee6db4a9cff9e212e3cacd216
Alexander Goetz <144852450+arbgoetz@users.noreply.github.com>
2025-07-17 16:11:14 -0700

    Update database.py
    changed sql server driver for compatibility with lab server
    added variant of fetch_data_from_sql that uses the "public" db role so unauthorized users have limited data access

--- database.py
+++ database.py
@@ -9,11 +9,47 @@ load_dotenv(override=True)
 def fetch_data_from_sql(query):
     """Fetch data from SQL Server using SQLAlchemy with PyODBC."""
     
-    driver = "ODBC Driver 17 for SQL Server"
+    driver = "ODBC Driver 18 for SQL Server"
     server = os.getenv("DB_SERVER")
     database = os.getenv("DB_DATABASE")
     username = os.getenv("DB_USERNAME")
     password = os.getenv("DB_PASSWORD")
+
+    if not all([server, database, username, password]):
+        raise ValueError("Missing one or more database environment variables.")
+
+    # SQLAlchemy connection string
+    connection_string = (
+        f"mssql+pyodbc://{username}:{password}@{server}/{database}"
+        f"?driver={driver}&Encrypt=yes&TrustServerCertificate=yes"
+    )
+
+    try:
+        # Use SQLAlchemy engine 
+        engine = create_engine(connection_string, fast_executemany=True)
+
+        with engine.connect() as connection:
+            df = pd.read_sql_query(query, connection)
+
+        return df
+
+    except Exception as e:
+        print(f"Database error: {e}")
+        return None
+
+    finally:
+        if 'engine' in locals() and engine:
+            engine.dispose()
+            
+            
+def fetch_data_from_sql_pub(query):
+    """Fetch data from SQL Server using SQLAlchemy with PyODBC."""
+    
+    driver = "ODBC Driver 18 for SQL Server"
+    server = os.getenv("DB_SERVER")
+    database = os.getenv("DB_DATABASE")
+    username = os.getenv("DB_USERNAME_PUB")
+    password = os.getenv("DB_PASSWORD_PUB")
 
     if not all([server, database, username, password]):
         raise ValueError("Missing one or more database environment variables.")
